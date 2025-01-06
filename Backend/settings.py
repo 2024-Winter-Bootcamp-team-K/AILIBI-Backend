8ac464babd7423ae7a0d25428bf49d258f604aaa
yeochi <yeochi2000@gmail.com>
2025-01-07 02:53:21 +0900

    update settings.py to use new DB

--- Backend/settings.py
+++ Backend/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'teamk',
+        'USER': 'team_k',
+        'PASSWORD': 'teamk123456!',
+        'HOST': 'db-team-k.cn0q48ww6mz8.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
