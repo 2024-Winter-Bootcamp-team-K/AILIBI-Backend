9425c28835b90158a29326ab033ccaff3ae1c151
박근채 <pgc0419@gmail.com>
2025-01-07 14:56:22 +0900

    feat: user/models.py setting

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=16)
+    email = models.CharField(max_length=32)
+    password = models.CharField(max_length=16)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'Users'
