e83765ab9b07f696efb16afa3cb814ce89a0660c
yeochi <yeochi2000@gmail.com>
2025-01-07 23:16:05 +0900

    if2

--- Backend/settings.py
+++ Backend/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = secret_data['DATABASE']
+DATABASES = secret_data['DATABASES']
 
 
 # Password validation
