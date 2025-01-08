db2ee2305323ea768173ffe3d2e8c865c73c783a
parksooyong03 <parkwaterdragon@tukorea.ac.kr>
2025-01-08 15:47:13 +0900

    STT APP 구현

--- Backend/urls.py
+++ Backend/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -35,6 +35,6 @@ urlpatterns = [
     path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path(r'swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path(r'redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
-
     path("admin/", admin.site.urls),
+    path('stt/', include('stt.urls')),
 ]
---
+++ stt/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.stt_process, name="stt_process"),
+
+]
