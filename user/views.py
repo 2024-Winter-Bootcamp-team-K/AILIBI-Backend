1927c977f07c0f03af8ebcb24eebaad2fbf64024
Grassyeochi <yeochi2000@gmail.com>
2025-01-08 17:09:29 +0900

    Task of login

--- user/views.py
+++ user/views.py
@@ -1,9 +1,10 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import UserSerializer
+from .serializers import UserSerializer, LoginSerializer
+from .models import User
 
-
+#회원 가입
 class UserRegistrationView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
@@ -14,3 +15,17 @@ class UserRegistrationView(APIView):
                 "email": serializer.validated_data["email"]
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#로그인
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            user = User.objects.get(email=email)  # 로그인 성공 시 사용자 정보
+            return Response({
+                "name": user.name,
+                "email": user.email
+            }, status=status.HTTP_201_CREATED)
+
+        return Response({"Error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
