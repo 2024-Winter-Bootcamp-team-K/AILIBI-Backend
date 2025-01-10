a7073e1534bad48f0287dbd3c2c18423b4a43439
박근채 <pgc0419@gmail.com>
2025-01-10 16:27:00 +0900

    refactor: chat 분리 성공

--- Backend/asgi.py
+++ Backend/asgi.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from llm.routing import websocket_urlpatterns
+from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.settings')
 
