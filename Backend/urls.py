67690d2b4e801d1ce8841b41d22d716b3ee47f79
Grassyeochi <yeochi2000@gmail.com>
2025-01-10 17:05:30 +0900

    make play_record_intergation and play_record_deleted

--- Backend/urls.py
+++ Backend/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -40,5 +40,6 @@ urlpatterns = [
     path('auth/', include('user.urls')),  # 사용자 로그인/회원가입
     path('users/', include('user.users_urls')),  # 사용자 정보 불러오기
 
-    path('histories/', include('scenario.urls'))
+    re_path(r'^histories\?scenario_id=\d+', include('scenario.urls')),  # scenario 관련 요청
+    re_path(r'^histories\?suspect_id=\d+', include('suspect.urls')),  # suspect 관련 요청
 ]
--- scenario/views.py
+++ scenario/views.py
@@ -2,14 +2,20 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-from .models import User, Scenario
-from .serializers import ScenarioSerializer, SelectedScenarioSerializer #, SuspectSerializer
+
+from .models import User
+from scenario.models import Scenario
+from chat.models import Chat
+from suspect.models import Suspect
+from evidence.models import Evidence
+
+from .serializers import ScenarioSerializer, SelectedScenarioSerializer
 
 class HistoriesView(APIView):
     def get(self, request):
         user_id = request.query_params.get('user_id')           #모든 플레이 기록 불러오기
         scenario_id = request.query_params.get('scenario_id')   #선택한 플레이 기록 불러오기
-        #suspect_id = request.query_params.get('suspect_id')     #선택한 용의자와 심문 내용 불러오기
+
 
         if user_id:
             user = get_object_or_404(User, id=user_id)
@@ -22,11 +28,21 @@ class HistoriesView(APIView):
             serializer = SelectedScenarioSerializer(scenario)
             return Response({"scenarios" : serializer.data}, status=status.HTTP_200_OK)
 
-
-        #elif suspect_id:
-        #    suspect = get_object_or_404(Suspect, id=suspect_id)
-        #    serializer = SuspectSerializer(suspect)
-        #    return Response(serializer.data, status=status.HTTP_200_OK)
-
         else:
             return Response({'error': '잘못된 요청'}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        scenario_id = request.query_params.get('scenario_id')
+
+        if scenario_id:
+            suspect_ids = Suspect.objects.filter(scenario_id=scenario_id).values_list('id', flat=True)
+
+            Scenario.objects.filter(id=scenario_id).delete()
+            Evidence.objects.filter(scenario_id=scenario_id).delete()
+            Suspect.objects.filter(scenario_id=scenario_id).delete()
+            Chat.objects.filter(suspect_id__in=suspect_ids).delete()
+
+            return Response({'message': '삭제 되었습니다.'},
+                            status=status.HTTP_200_OK)
+
+        return Response({'error': 'scenario_id is required for deletion'}, status=status.HTTP_400_BAD_REQUEST)
