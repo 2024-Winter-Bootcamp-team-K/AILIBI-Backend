c25f54dd48a700275a1ad7e8e43f01bd82d044e6
박근채 <pgc0419@gmail.com>
2025-01-21 18:50:35 +0900

    fix: chat api url fix

--- chat/urls.py
+++ chat/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # WebSocket 테스트용 URL
     path("/test-websocket", websocket_test, name="websocket_test"),
     path('/websocket/connect', WebSocketConnectAPIView.as_view(), name='websocket_connect'),
-    path('/<int:suspects_id>', WebSocketMessageAPIView.as_view(), name='websocket_message'),
+    path('/<int:suspect_id>', WebSocketMessageAPIView.as_view(), name='websocket_message'),
     path('/websocket/status', WebSocketStatusAPIView.as_view(), name='websocket_status'),
 ]
