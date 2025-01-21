828704c6e47e8d343c642171a1089f82aa424483
박근채 <pgc0419@gmail.com>
2025-01-21 18:35:19 +0900

    fix: chat api url fix

--- chat/urls.py
+++ chat/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from .views import websocket_test, WebSocketConnectAPIView, WebSocketMessageAPIView, WebSocketStatusAPIView
 
-
 urlpatterns = [
     # WebSocket 테스트용 URL
     path("/test-websocket", websocket_test, name="websocket_test"),
     path('/websocket/connect', WebSocketConnectAPIView.as_view(), name='websocket_connect'),
-    path('/websocket/message', WebSocketMessageAPIView.as_view(), name='websocket_message'),
+    path('/<int:suspects_id>', WebSocketMessageAPIView.as_view(), name='websocket_message'),
     path('/websocket/status', WebSocketStatusAPIView.as_view(), name='websocket_status'),
 ]
