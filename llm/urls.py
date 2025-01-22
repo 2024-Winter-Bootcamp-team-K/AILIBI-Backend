0d2b16871bcd68073b6f924af00a42de524e49d6
박근채 <pgc0419@gmail.com>
2025-01-23 02:14:32 +0900

    refactor: url 빼 먹었네 잇힝

--- llm/urls.py
+++ llm/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from .views import ScenarioAPIView, GenerateEvidenceAPIView, GenerateSuspectAPIView
 
 urlpatterns = [
-    path('', views.ScenarioAPIView.as_view(), name='create_scenario'),
+    path('/create', ScenarioAPIView.as_view(), name='create_scenario'),
+    path('/evidence', GenerateEvidenceAPIView.as_view(), name='generate_evidence'),
+    path('/suspect', GenerateSuspectAPIView.as_view(), name='generate_suspect'),
 ]
